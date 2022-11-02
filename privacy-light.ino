9680649f62c7f649bcdd08ca59f06a804a9a559d
Kenneth Mason <kennymmase@gmail.com>
2022-11-02 16:51:57 -0400

    Major refactoring

--- privacy-light.ino
+++ privacy-light.ino
@@ -4,14 +4,31 @@
 
 // Privacy Light - A Do-Not-Disturb Indicator
 
-// colors mapped to keys
-//numeric keys are hardcoded expected input from the IR remote I'm using to test (could differ with different remotes possibly)
-uint32_t ylwKey = 16718055;
-uint32_t redKey = 16718565;
-uint32_t grnKey = 16751205;
-uint32_t bluKey = 16753245;
+// Define a few colors we can set up as presets
+enum BacklightColor { 
+  BL_UNDEF,
+  BL_RED,
+  BL_ORANGE,
+  BL_YELLOW,
+  BL_GREEN,
+  BL_BLUE,
+  BL_PURPLE,
+  BL_PINK,
+  BL_WHITE
+};
 
-// init for IR
+BacklightColor currentColor = BL_UNDEF;
+
+// Colors mapped to IR codes
+//numeric keys are hardcoded expected input from the IR remote I'm using to test (could differ with different remotes possibly)
+const uint32_t ylwKey = 16718055;
+const uint32_t redKey = 16718565;
+const uint32_t grnKey = 16751205;
+const uint32_t bluKey = 16753245;
+const uint32_t whtKey = 16720605;
+const uint32_t pnkKey = 16734375;
+
+// init for IR receiver
 int IrReceiverPin = 12;
 IRrecv irrecv(IrReceiverPin);
 decode_results results;
@@ -21,22 +38,28 @@ Waveshare_LCD1602_RGB lcd(16,2);  //16 characters and 2 lines of show
 int r,g,b, t = 0;
 
 // LED
-const int button = 3;
 const int red = 11;
 const int green = 10;
 const int blue = 9;
-int counter = 0;
-int buttonState = 0;
-int lastButtonState = 0;
 
 // Delays
-unsigned long startSleepTimer = 0; // the time the delay started
-bool awake = false; // true if still waiting for delay to finish
+unsigned long startSleepTimer = 0; // when the timer started
+bool awake = false; // if true, display is on and timer until sleep is running
 
-unsigned long redDelayStart = 0; // the time the delay started
+unsigned long fadeDelay = 0; // the time we last updated the backlight
+unsigned long scrollDelay = 0; // the time we last updated text
+
+// Text to use for each preset
+const char noneText[2][17] = {"     Hello      ", "      World!    "};
+const char yellowText[2][17] = {"   On a call.   ", "Or on the phone."};
+const char greenText[2][17] = {"  Come on in!   ", "  But... knock  "};
+const char blueText[2][17] = {"  Bedtime Boii  ", "   Goodnight!   "};
+const char redText[4][17] = {"   Stay Out!!   ", "                ", " Or risk waking ", "    the bear.   "};
+const char pinkText[2][17] = {"  Do Not Come   ", "    Inside!!!   "};
+const char whiteText[2][17] = {"   Come Party   ", " I'm having fun "};
+
+// Indicates the current page for presets with changing text
 int page = 0;
-
-String currentColor = "none";
 
 void setup() {
   // IR
@@ -65,60 +88,62 @@ void setup() {
   lcd.send_string("World");
 
   // LED
-  pinMode(button, INPUT);
   pinMode(red, OUTPUT);
   pinMode(green, OUTPUT);
   pinMode(blue, OUTPUT);
 
   // Starting delay
-  startSleepTimer = millis();   // start delay
-  awake = true; // not finished yet
+  startSleepTimer = millis(); // start countdown to sleep
+  awake = true; // display is on
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if (irrecv.decode(&results)){
     Serial.println(results.value);
-
-    if (results.value == redKey) {
-      onRed();
+    switch (results.value) {
+      case grnKey:
+        currentColor = BL_GREEN;
+        break;
+      case ylwKey:
+        currentColor = BL_YELLOW;
+        break;
+      case redKey:
+        currentColor = BL_RED;
+        break;
+      case pnkKey:
+        currentColor = BL_PINK;
+        break;
+      case bluKey:
+        currentColor = BL_BLUE;
+        break;
+      case whtKey:
+        currentColor = BL_WHITE;
+        break;
+      default:
+        irrecv.resume();
+        return;
     }
 
-    else if (results.value == ylwKey) {
-      onYellow();
-    }
-
-    else if (results.value == grnKey) {
-      onGreen();
-    }
-
-    else if (results.value == bluKey) {
-      onBlue();
-    }
+    wake();
 
     irrecv.resume();
   }
   
-  if (currentColor == "red") {
-    scrollRed();
-  }
-  else if (currentColor == "none" && awake) {
-    r = (abs(sin(3.14*t/180)))*255;
-    g = (abs(sin(3.14*(t + 60)/180)))*255;
-    b = (abs(sin(3.14*(t + 120)/180)))*255;
-    t = t + 3;
-    lcd.setRGB(r,g,b);
-    
-    delay(110);
-  }
-  
-  if (awake && millis() - startSleepTimer >= 20000) {
-    sleep();
+  if (awake) {
+    if (millis() - startSleepTimer < 20000) {
+      // stay awake, re-render
+      renderScroll();
+      renderFade();
+    }
+    else {
+      sleep();
+    }
   }
   
   // just until we hook up sensor
   if (!awake && millis() - startSleepTimer >= 20000) {
-     wake();
+    wake();
   }
   
 //  // Delay a little bit to avoid bouncing
@@ -137,127 +162,117 @@ void sleep () {
 }
 
 void wake () {
-  if (!awake) {
-    awake = true;
-    lcd.display();
-  }
-
+  awake = true;
   startSleepTimer = millis();
-  
+
+  t = 0;
+  page = 0;
+  setDisplay();
+  lcd.display();
+}
+
+
+void setLED (uint8_t r, uint8_t g, uint8_t b) {
+  digitalWrite(red, r);
+  digitalWrite(green, g);
+  digitalWrite(blue, b);
+}
+
+void setColors (float pr, float pg, float pb) {
+  r = (pr)*255;
+  g = (pg)*255;
+  b = (pb)*255;
+
   lcd.setRGB(r,g,b);
 }
 
-void onYellow() {
-  currentColor = "yellow";
-  
-  // setting LCD
+void setText (char* top, char* bottom) {
   lcd.setCursor(0,0);
-  lcd.send_string("   On a call.   ");
+  lcd.send_string(top);
   lcd.setCursor(0,1);
-  lcd.send_string("Or on the phone.");
-
-  // setting LED
-  digitalWrite(red, HIGH);
-  digitalWrite(green, HIGH);
-  digitalWrite(blue, LOW);
-  
-  r = (1)*255;
-  g = (0.3)*255;
-  b = (0)*255;
-
-  // setRGB and restart sleep timer
-  wake();
+  lcd.send_string(bottom);
 }
 
-void onGreen() {
-  currentColor = "green";
+void setDisplay () {
+  int index = page * 2;
   
-  // setting LCD
-  lcd.setCursor(0,0);
-  lcd.send_string("  Come on in!   ");
-  lcd.setCursor(0,1);
-  lcd.send_string("  But... knock  ");
+  switch (currentColor) {
+    case BL_UNDEF:
+    case BL_WHITE:
+      // renderFade() sets the RGB backlight, so we don't have to call setColors()
+      renderFade();
 
-  // setting LED
-  digitalWrite(green, HIGH);
-  digitalWrite(red, LOW);
-  digitalWrite(blue, LOW);
-  
-  r = (0)*255;
-  g = (1)*255;
-  b = (0)*255;
-  
-  // setRGB and restart sleep timer
-  wake();
+      setText(whiteText[0], whiteText[1]);
+      setLED(HIGH, HIGH, HIGH);
+      break;
+    case BL_GREEN:
+      setColors(0, 1, 0);
+      setText(greenText[0], greenText[1]);
+      setLED(LOW, HIGH, LOW);
+      break;
+    case BL_YELLOW:
+      setColors(1, 0.3, 0);
+      setText(yellowText[0], yellowText[1]);
+      setLED(HIGH, HIGH, LOW);
+      break;
+    case BL_RED:
+      setColors(1, 0, 0);
+      setText(redText[0], redText[1]);
+      setLED(HIGH, LOW, LOW);
+
+      // set the scrollDelay when starting a preset with multiple pages of text
+      scrollDelay = millis();
+      break;
+    case BL_PINK:
+      setColors(0.85, 0, 0.5);
+      setText(pinkText[0], pinkText[1]);
+      setLED(HIGH, LOW, HIGH);
+      break;
+    case BL_BLUE:
+      setColors(0.3, 0, 1);
+      setText(blueText[0], blueText[1]);
+      setLED(LOW, LOW, HIGH);
+      break;
+    default:
+      return;
+  }
 }
 
-void onBlue() {
-  currentColor = "blue";
-  
-  // setting LCD
-  lcd.setCursor(0,0);
-  lcd.send_string("  Bedtime Boii  ");
-  lcd.setCursor(0,1);
-  lcd.send_string("   Goodnight!   ");
+// unique cases must be made for each color you want to set a scroll for
+void renderScroll () {
+  //TODO: dynamically tell if a color should scroll without being hardcoded
+    // if colorText.size > 2, scroll; if size % 2 == 1, on last page setText(colorText[size], <16 space chars>);
+  if (currentColor == BL_RED) {
+    int dur = millis() - scrollDelay;
 
-  // setting LED
-  digitalWrite(blue, HIGH);
-  digitalWrite(red, LOW);
-  digitalWrite(green, LOW);
-  
-  r = (0.3)*255;
-  g = (0)*255;
-  b = (1)*255;
+    if (dur <= 6500) {
+      page = 0;
+    }
+    else if (dur > 6500 && dur < 10000) {
+      page = 1;
+    }
+    else {
+      page = 0;
+      scrollDelay = millis();
+    }
 
-  // setRGB and restart sleep timer
-  wake();
+    setText(redText[(page * 2)], redText[(page * 2) + 1]);
+  }
 }
 
-void onRed() {
-  currentColor = "red";
-  
-  // setting LCD
-  redDelayStart = millis();
-  page = -1;
-  scrollRed();
-  
-  // setting LED
-  digitalWrite(red, HIGH);
-  digitalWrite(green, LOW);
-  digitalWrite(blue, LOW);
-  
-  // setRGB and restart sleep timer
-  wake();
-}
-
-void scrollRed() {
-  // setting LCD
-  r = (1)*255;
-  g = (0)*255;
-  b = (0)*255;
+void renderFade () {
+  if (currentColor != BL_UNDEF && currentColor != BL_WHITE) {
+    return;
+  }
 
   // unsigned subtraction is always positive, so even when millis() reaches its max (4,294,967,295)
-  // and resets to 0, (millis() - redDelayStart) >= 10000) will still always work
-  int dur = millis() - redDelayStart;
+  // and resets to 0, (millis() - delayStart) >= 10000) will still always work
+  int dur = millis() - fadeDelay;
   
-  if (page == -1 || (page != 0 && dur <= 7000)) {
-    lcd.setCursor(0,0);
-    lcd.send_string("   Stay Out!!   ");
-    lcd.setCursor(0,1);
-    lcd.send_string("                ");
-
-    page = 0;
-  }
-  else if (page != 1 && dur > 7000) {
-    lcd.setCursor(0,0);
-    lcd.send_string(" Or risk waking ");
-    lcd.setCursor(0,1);
-    lcd.send_string("    the bear.   ");
-
-    page = 1;
-  }
-  
-  if (dur >= 10000) {
-    redDelayStart = millis();
+  if (dur >= 100) {
+    fadeDelay = millis();
+    
+    setColors((abs(sin(3.14*t/180))), (abs(sin(3.14*(t + 60)/180))), (abs(sin(3.14*(t + 120)/180))));
+    t = t + 3;
   }
 }
